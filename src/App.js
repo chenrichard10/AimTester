1dbd21de10cedfebd5958891747a29e9628b06f0
Richard Chen <chenpipi@live.com>
2020-10-12 12:14:53 -0400

    Semi-working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useRef, useState, Suspense} from 'react'
+import React, { useRef, useState, useEffect} from 'react'
 import { Canvas, useFrame, useLoader } from 'react-three-fiber'
 import { Html, draco } from 'drei'
 import './styles.css'
 import {gameOver, generatePosition, checkScore} from './game'
-import Stopwatch from './StopWatch'
+import Timer from 'react-compound-timer'
 
 function Box(props) {
   // This reference will give us direct access to the mesh
@@ -30,6 +30,7 @@ function Box(props) {
   )
 }
 
+
 function ScoreBoard(props) { 
   return (
     <div> 
@@ -38,45 +39,83 @@ function ScoreBoard(props) {
   )
 }
 
+
 function PlayAgain(props) { 
-  const [again, setAgain] = React.useState(false);
   return (
-      <button onClick={setAgain(true)}> Play Again </button>
+    <button onClick={() => props.reset(true)}>
+        Play Again
+    </button>
   )
 }
 
 
-function Game(props) { 
-  const [showBox, setShowBox] = React.useState(true)
-  const [count, setCount] = useState(0)
+  function Game(props) { 
+    const [showBox, setShowBox] = React.useState(true)
+    const [count, setCount] = useState(0)
+    const [repeat, setRepeat] = useState(1)
 
-  const handleClick = () => {
-    setShowBox(!showBox)
-    setCount(count + 1)
+    const handleClick = () => {
+      setShowBox(!showBox)
+      setCount(count + 1)
+    }
+
+    useEffect(() => {
+      if (count === 10) {
+        props.pause();
+      }  
+    }, [count, props.pause]);
+    
+
+    useEffect(() => {
+      if (count === 10) {
+        setRepeat(0);
+      }  
+    }, [count, setRepeat]); 
+
+
+    useEffect(() => {
+      if (props.again) {
+        setCount(0);
+        props.reset();
+      }
+    }, [props.again, props.reset]);
+
+    return (
+      <> 
+    <ScoreBoard score={count}/>
+      {gameOver(count) && repeat ?
+      (<Canvas>
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} /> 
+        {showBox && <Box onClick={handleClick} position={generatePosition()} />}
+        {!showBox && <Box onClick={handleClick} position={generatePosition()} />}
+      </Canvas>)  : (<div> <h1> Game Over!</h1></div>) }
+      </>
+    )
   }
 
-  return (
-    <> 
-   <Stopwatch/>
-   <ScoreBoard score={count}/>
-   <playAgain/>
-    {gameOver(count) ?
-    (<Canvas>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} /> 
-      {showBox && <Box onClick={handleClick} position={generatePosition()} />}
-      {!showBox && <Box onClick={handleClick} position={generatePosition()} />}
-    </Canvas>)  : (<div> <h1> Game Over!</h1></div>) }
-    </>
-  )
-}
-
-
-
 export default function App() {
+  const [isReset, setisReset] = React.useState(false);
   return (
-    <Game />
+    <Timer initialTime={0}>
+      {({ start, resume, pause, stop, reset})  => (
+      
+      <React.Fragment>
+        <div>
+            <Timer.Seconds /> seconds, 
+            <Timer.Milliseconds /> milliseconds
+        </div>
+        <div>
+            <button onClick={reset}> New </button>
+        </div>
+        <PlayAgain reset={setisReset}/>
+        <Game pause={pause} reset={reset} again={isReset}/>
+        
+    </React.Fragment>)
+    }
+    </Timer>
+   
   )
 }
 
